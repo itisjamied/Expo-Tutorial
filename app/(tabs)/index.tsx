010f76e1cff4ced02727fc6e10d27c438fa3a507
Jamie <77027304+itisjamied@users.noreply.github.com>
2025-12-12 13:36:43 -0500

    Created Button component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,25 +1,37 @@
-import { StyleSheet, View } from "react-native";
-import ImageViewer from "../components/ImageViewer";
+import { StyleSheet, View } from 'react-native';
 
-const PlaceholderImage = require('@/assets/images/background-image.png');
+import Button from '@/components/Button';
+import ImageViewer from '@/components/ImageViewer';
+
+const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage}></ImageViewer>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" theme='primary' />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor: "#25292e",
-    alignItems:"center",
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
   },
-  imageContainer:{
-    flex:1,
+  imageContainer: {
+    flex: 1,
+    paddingTop: 28,
+    marginBottom: 400, 
   },
-})
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+});
