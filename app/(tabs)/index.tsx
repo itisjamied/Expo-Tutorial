2a897a8d1ea7a8f54d413d4506437337b13339a4
Jamie <77027304+itisjamied@users.noreply.github.com>
2025-12-12 13:53:23 -0500

    Open up image library

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,17 +2,30 @@ import { StyleSheet, View } from 'react-native';
 
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
+import * as ImagePicker from 'expo-image-picker';
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  }
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button label="Choose a photo" theme='primary' />
+        <Button label="Choose a photo" theme='primary' onPress={pickImageAsync} />
         <Button label="Use this photo" />
       </View>
     </View>
@@ -28,7 +41,6 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
     paddingTop: 28,
-    marginBottom: 400, 
   },
   footerContainer: {
     flex: 1 / 3,
